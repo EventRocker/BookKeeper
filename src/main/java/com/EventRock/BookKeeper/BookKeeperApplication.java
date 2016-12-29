3b7da79e034e1c5057cd29ce4b74a393712ad4c8
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-29 10:37:49 +0700

    Init Project

---
+++ src/main/java/com/EventRock/BookKeeper/BookKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.EventRock.BookKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKeeperApplication.class, args);
+	}
+}
