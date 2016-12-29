21ef7d4f317a3d71f7c18eb1ec3d4c9ee25da6ac
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-29 11:32:29 +0700

    [Booking] Create bookings database by liquibase

--- src/test/java/com/EventRock/BookKeeper/BookKeeperApplicationTests.java
+++ src/test/java/com/EventRock/BookKeeper/BookKeeperApplicationTests.java
@@ -1,16 +1,16 @@
-//package com.EventRock.BookKeeper;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class BookKeeperApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.EventRock.BookKeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("h2")
+public class BookKeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
