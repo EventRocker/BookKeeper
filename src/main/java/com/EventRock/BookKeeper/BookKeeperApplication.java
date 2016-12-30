58326ac90af488310adc8f469dca048dca87b8b6
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-30 11:03:07 +0700

    Add Hello world

--- src/main/java/com/EventRock/BookKeeper/BookKeeperApplication.java
+++ src/main/java/com/EventRock/BookKeeper/BookKeeperApplication.java
@@ -2,11 +2,20 @@ package com.EventRock.BookKeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookKeeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookKeeperApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello EventRock!";
+	}
+
 }
